9024052ca65c66292a0caa8c48b0c23c3b3dd9ac
Adi <buziaki@gmaim.com>
2019-05-18 23:03:34 +0200

    zc

---
+++ dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [aseaae] NCHAR(10) NULL,
+    [aseaae] NCHAR(10) NULL,
+    [aseaae] NCHAR(10) NULL,
+    [aseaae] NCHAR(10) NULL,
+    [aseaae] NCHAR(10) NULL
+)
